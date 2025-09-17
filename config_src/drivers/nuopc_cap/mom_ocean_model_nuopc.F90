f733422e675783a0f75c310e695ecf77a06def25
Claire Yung <u6066740@anu.edu.au>
2025-09-17 17:17:22 +1000

    Hardcoded name changes to allow ice shelf to restart with NUOPC.
    See also https://github.com/ACCESS-NRI/MOM6/issues/29
    
    Hardcode ice shelf restart to be in 'RESTART/' in nuopc cap. Also hardcodes the name of the ice shelf restart Shelf.res.nc in initialize_ice_shelf.

--- config_src/drivers/nuopc_cap/mom_ocean_model_nuopc.F90
+++ config_src/drivers/nuopc_cap/mom_ocean_model_nuopc.F90
@@ -772,7 +772,7 @@ subroutine ocean_model_restart(OS, timestamp, restartname, stoch_restartname, nu
          OS%dirs%restart_output_dir) ! Is this needed?
     if (OS%use_ice_shelf) then
       call ice_shelf_save_restart(OS%Ice_shelf_CSp, OS%Time, &
-           OS%dirs%restart_output_dir)
+            './RESTART/') !OS%dirs%restart_output_dir)
     endif
   else
     if (BTEST(OS%Restart_control,1)) then
@@ -781,7 +781,7 @@ subroutine ocean_model_restart(OS, timestamp, restartname, stoch_restartname, nu
       call forcing_save_restart(OS%forcing_CSp, OS%grid, OS%Time, &
            OS%dirs%restart_output_dir, time_stamped=.true.)
       if (OS%use_ice_shelf) then
-        call ice_shelf_save_restart(OS%Ice_shelf_CSp, OS%Time, OS%dirs%restart_output_dir, .true.)
+        call ice_shelf_save_restart(OS%Ice_shelf_CSp, OS%Time, './RESTART/', .true.) ! OS%dirs%restart_output_dir, .true.)
       endif
     endif
     if (BTEST(OS%Restart_control,0)) then
@@ -790,7 +790,7 @@ subroutine ocean_model_restart(OS, timestamp, restartname, stoch_restartname, nu
       call forcing_save_restart(OS%forcing_CSp, OS%grid, OS%Time, &
            OS%dirs%restart_output_dir)
       if (OS%use_ice_shelf) then
-        call ice_shelf_save_restart(OS%Ice_shelf_CSp, OS%Time, OS%dirs%restart_output_dir)
+        call ice_shelf_save_restart(OS%Ice_shelf_CSp, OS%Time, './RESTART/') !OS%dirs%restart_output_dir)
       endif
     endif
   endif
@@ -851,7 +851,7 @@ subroutine ocean_model_save_restart(OS, Time, directory, filename_suffix)
   call forcing_save_restart(OS%forcing_CSp, OS%grid, Time, restart_dir)
 
   if (OS%use_ice_shelf) then
-    call ice_shelf_save_restart(OS%Ice_shelf_CSp, OS%Time, OS%dirs%restart_output_dir)
+    call ice_shelf_save_restart(OS%Ice_shelf_CSp, OS%Time, './RESTART/') ! OS%dirs%restart_output_dir)
   endif
 end subroutine ocean_model_save_restart
 
