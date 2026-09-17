3c33692ab52ef94590ff0acaa2ead520c7af32b0
minghangli-uni <24727729+minghangli-uni@users.noreply.github.com>
2026-09-17 10:35:43 +1000

    skip masked cells in spatial reductions
    Masked land values may contain fill values or nans. Dont reply on multiplication by a zero mask when forming reproducible diagnostic sums, and exclude land cells explicitly from ocean mass diagnostics.

--- src/diagnostics/MOM_diagnostics.F90
+++ src/diagnostics/MOM_diagnostics.F90
@@ -340,7 +340,8 @@ subroutine calculate_diagnostic_fields(u, v, h, uh, vh, tv, ADp, CDp, p_surf, &
   if (CS%id_masso > 0) then
     mass_cell(:,:) = 0.0
     do k=1,nz ; do j=js,je ; do i=is,ie
-      mass_cell(i,j) = mass_cell(i,j) + (GV%H_to_RZ*h(i,j,k)) * G%areaT(i,j)
+      if (G%mask2dT(i,j) > 0.0) &
+        mass_cell(i,j) = mass_cell(i,j) + (GV%H_to_RZ*h(i,j,k)) * G%areaT(i,j)
     enddo ; enddo ; enddo
     masso = reproducing_sum(mass_cell, unscale=US%RZL2_to_kg)
     call post_data(CS%id_masso, masso, CS%diag)
--- src/diagnostics/MOM_spatial_means.F90
+++ src/diagnostics/MOM_spatial_means.F90
@@ -78,7 +78,10 @@ function global_area_mean(var, G, scale, tmp_scale, unscale)
 
   tmpForSumming(:,:) = 0.
   do j=js,je ; do i=is,ie
-    tmpForSumming(i,j) = var(i,j) * (scalefac * G%areaT(i,j) * G%mask2dT(i,j))
+    if (G%mask2dT(i,j) > 0.0) then
+      tmpForSumming(i,j) = var(i,j) * &
+          (scalefac * G%areaT(i,j) * G%mask2dT(i,j))
+    endif
   enddo ; enddo
 
   global_area_mean = reproducing_sum(tmpForSumming, unscale=temp_scale*G%US%L_to_m**2) * G%IareaT_global
@@ -227,7 +230,10 @@ function global_area_integral(var, G, scale, area, tmp_scale, unscale)
     enddo ; enddo
   else
     do j=js,je ; do i=is,ie
-      tmpForSumming(i,j) = var(i,j) * (scalefac * G%areaT(i,j) * G%mask2dT(i,j))
+      if (G%mask2dT(i,j) > 0.0) then
+        tmpForSumming(i,j) = var(i,j) * &
+            (scalefac * G%areaT(i,j) * G%mask2dT(i,j))
+      endif
     enddo ; enddo
   endif
 
@@ -306,8 +312,10 @@ function global_layer_mean(var, h, G, GV, scale, tmp_scale, unscale)
   tmpForSumming(:,:,:) = 0. ; weight(:,:,:) = 0.
 
   do k=1,nz ; do j=js,je ; do i=is,ie
-    weight(i,j,k)  =  (GV%H_to_MKS * h(i,j,k)) * (G%areaT(i,j) * G%mask2dT(i,j))
-    tmpForSumming(i,j,k) =  scalefac * var(i,j,k) * weight(i,j,k)
+    if (G%mask2dT(i,j) > 0.0) then
+      weight(i,j,k) = (GV%H_to_MKS * h(i,j,k)) * (G%areaT(i,j) * G%mask2dT(i,j))
+      tmpForSumming(i,j,k) = scalefac * var(i,j,k) * weight(i,j,k)
+    endif
   enddo ; enddo ; enddo
 
   global_temp_scalar = reproducing_sum(tmpForSumming, EFP_lay_sums=laysums(1:nz), only_on_PE=.true., &
@@ -373,9 +381,11 @@ function global_volume_mean(var, h, G, GV, scale, tmp_scale, unscale)
   tmpForSumming(:,:) = 0. ; sum_weight(:,:) = 0.
 
   do k=1,nz ; do j=js,je ; do i=is,ie
-    weight_here  =  (GV%H_to_MKS * h(i,j,k)) * (G%areaT(i,j) * G%mask2dT(i,j))
-    tmpForSumming(i,j) = tmpForSumming(i,j) + scalefac * var(i,j,k) * weight_here
-    sum_weight(i,j) = sum_weight(i,j) + weight_here
+    if (G%mask2dT(i,j) > 0.0) then
+      weight_here  =  (GV%H_to_MKS * h(i,j,k)) * (G%areaT(i,j) * G%mask2dT(i,j))
+      tmpForSumming(i,j) = tmpForSumming(i,j) + scalefac * var(i,j,k) * weight_here
+      sum_weight(i,j) = sum_weight(i,j) + weight_here
+    endif
   enddo ; enddo ; enddo
   global_volume_mean = (reproducing_sum(tmpForSumming, unscale=temp_scale*G%US%L_to_m**2)) / &
                        (reproducing_sum(sum_weight, unscale=G%US%L_to_m**2))
@@ -458,13 +468,17 @@ function global_mass_integral(h, G, GV, var, on_PE_only, scale, tmp_scale, unsca
   tmpForSumming(:,:) = 0.0
   if (present(var)) then
     do k=1,nz ; do j=js,je ; do i=is,ie
-      tmpForSumming(i,j) = tmpForSumming(i,j) + var(i,j,k) * &
-                ((GV%H_to_RZ * h(i,j,k)) * (scalefac*G%areaT(i,j) * G%mask2dT(i,j)))
+      if (G%mask2dT(i,j) > 0.0) then
+        tmpForSumming(i,j) = tmpForSumming(i,j) + var(i,j,k) * &
+            ((GV%H_to_RZ * h(i,j,k)) * (scalefac*G%areaT(i,j) * G%mask2dT(i,j)))
+      endif
     enddo ; enddo ; enddo
   else
     do k=1,nz ; do j=js,je ; do i=is,ie
-      tmpForSumming(i,j) = tmpForSumming(i,j) + &
-                ((GV%H_to_RZ * h(i,j,k)) * (scalefac*G%areaT(i,j) * G%mask2dT(i,j)))
+      if (G%mask2dT(i,j) > 0.0) then
+        tmpForSumming(i,j) = tmpForSumming(i,j) + &
+            ((GV%H_to_RZ * h(i,j,k)) * (scalefac*G%areaT(i,j) * G%mask2dT(i,j)))
+      endif
     enddo ; enddo ; enddo
   endif
   global_sum = .true. ; if (present(on_PE_only)) global_sum = .not.on_PE_only
